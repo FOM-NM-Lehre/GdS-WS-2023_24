88e81136f398594a58f1dba1d266695c198fb13e
NMarkgraf <nmarkgraf@hotmail.com>
2023-09-04 09:29:48 +0200

    Pakete-installieren.R nachgereicht.

---
+++ setup/Pakete-installieren.R
@@ -0,0 +1,5 @@
+# Installiert die nötigen R Pakete
+install.packages(c("mosaic", "lsr", "GGally", "corrplot", 
+                   "googlesheets4", "readxl", "here",
+                   "tinytex", "rmarkdown", "knitr",
+                   "markdown","shiny", "gridExtra"))
